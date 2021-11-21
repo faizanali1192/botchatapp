fa25e6b19abc660b0c269e11e1f90e6ad7d9048d
faizanali1192 <faizanit1192@gmail.com>
2021-11-21 12:45:07 +0500

    first commit

---
+++ components/Chat.js
@@ -0,0 +1,70 @@
+import React,{useState,useEffect} from 'react'
+import { View, Text,TextInput,Button ,StyleSheet } from 'react-native'
+
+export  function Chat({route,navigation}) {
+    const [message, setMessage] = useState("")
+    const [messages, setMessages] = useState([])
+
+
+
+const{id}=route.params;
+    const{name}=route.params; 
+
+    const botMessage=["i am Bot"]
+
+    const sendMessage=(e)=>{
+        e.preventDefault();
+        setMessages([...messages,message,"Bot"])
+
+    }
+
+    // const sendBotMessage=()=>{
+    //     setMessage([...messages,"i am a bot"])
+    // }
+
+    console.log(message)
+    return (
+        <View>
+           <Text 
+         
+           onPress={()=>navigation.navigate('Chat')}>{name} </Text>
+           {messages.map((m)=>{
+               return <Text   style={styles.userMessage} >{m}</Text>
+           })}
+           <TextInput
+           style={{ 
+    	   height: 40, 
+            borderColor: 'gray', 
+            borderWidth: 1,
+            placeholderTextColor: 'gray',
+    }}
+      onChangeText={message => setMessage(message)}
+      value={message}
+	  placeholder="Enter Your Message..."
+    />
+    <Button 
+title="click me"
+onPress={(e) => {sendMessage(e)}}
+/>
+        </View>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    userMessage:{
+        border:"2px solid green",
+
+    },
+    container: {
+      marginTop: 50,
+    },
+    bigBlue: {
+      color: 'blue',
+      fontWeight: 'bold',
+      fontSize: 30,
+    },
+    red: {
+      color: 'red',
+    },
+  });
---
+++ components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export  function Home({navigation}) {
+const users=[
+
+    {id:1,name:"Faizan"},
+    {id:2,name:"Hassan"}
+]
+    return (
+        <View>
+           {users.map((user)=>{
+             return  <Text onPress={()=>navigation.navigate('Chat',{id:user.id,name:user.name})} >{user.name}</Text>
+           })}
+        </View>
+    )
+}
