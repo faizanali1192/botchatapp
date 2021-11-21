d2acbb5a5960284a92492b59dda14b08037433a5
faizanali1192 <faizanit1192@gmail.com>
2021-11-21 17:02:44 +0500

    fifth commit

--- components/Chat.js
+++ components/Chat.js
@@ -1,82 +1,76 @@
-import React,{useState,useEffect} from 'react'
-import { View, Text,TextInput,Button ,StyleSheet, ScrollView } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { View, Text, TextInput, Button, StyleSheet, ScrollView } from 'react-native'
 
-export  function Chat({route,navigation}) {
+export function Chat({ route, navigation }) {
     const [message, setMessage] = useState("")
     const [messages, setMessages] = useState([])
 
-const{id}=route.params; 
-    const{name}=route.params; 
+    const { id } = route.params;
+    const { name } = route.params;
 
-    const botMessage=["i am Bot"]
+    const botMessage = ["i am Bot"]
 
-    const sendMessage=(e)=>{
+    const sendMessage = (e) => {
         e.preventDefault();
-        setMessages([...messages,message,botMessage[0]])
+        setMessages([...messages, message, botMessage[0]])
         setMessage("");
     }
 
-
-
-    console.log(message)
     return (
-        <View style={{backgroundColor:"lightgrey", flex:1, paddingLeft:10, paddingRight:10, }} >
-         <ScrollView>
-           <Text 
-         
-           onPress={()=>navigation.navigate('Chat')}>{name} </Text>
-           {messages.map((m,index)=>{
-               return <Text key={index}  style={[m=="i am Bot"?  styles.botmessage : styles.usermessage]} >{m}</Text>
-           })}
-</ScrollView>
-
-
-           <View style={{display:"flex", flexDirection:"row" ,justifyContent:"center" ,marginTop:"auto", display:"inlineBlock"}}>
-           <TextInput
-           style={{ 
-    	   height: 40, 
-            borderColor: 'gray', 
-            borderWidth: 1,
-            placeholderTextColor: 'gray',
-        //    marginTop:"auto",
-           width:"100%",
-           padding:5,
+        <View style={{ backgroundColor: "lightgrey", flex: 1, paddingLeft: 10, paddingRight: 10, }} >
            
-    }}
-      onChangeText={message => setMessage(message)}
-      value={message}
-	  placeholder="Enter Your Message..."
-    />
-    <Button 
-    style={{
-        height: 40, 
-         borderColor: 'gray', 
-         borderWidth: 1,
-         placeholderTextColor: 'gray',
+            <ScrollView>
+                <Text
+                onPress={() => navigation.navigate('Chat')}>{name} </Text>
+                {messages.map((m, index) => {
+                    return <Text key={index} style={[m == "i am Bot" ? styles.botmessage : styles.usermessage]} >{m}</Text>
+                })}
+            </ScrollView>
 
-           
- }}
-title="Send"
-onPress={(e) => {sendMessage(e)}}
-/>
-</View>
+
+            <View style={{ display: "flex", flexDirection: "row", justifyContent: "center", marginTop: "auto", display: "inlineBlock" }}>
+              
+                <TextInput
+                    style={styles.textfield}
+                    onChangeText={message => setMessage(message)}
+                    value={message}
+                    placeholder="Enter Your Message..."
+                />
+
+                <Button
+                    style={styles.btn}
+                    title="Send"
+                    onPress={(e) => { sendMessage(e) }}
+                />
+                
+            </View>
         </View>
     )
 }
 
 
 const styles = StyleSheet.create({
-
-    usermessage:{
-       color:"green",
-        marginLeft:"auto",
-        fontSize:20
-
+    usermessage: {
+        color: "green",
+        marginLeft: "auto",
+        fontSize: 20
     },
-    botmessage:{
-       color:"orange",
-       fontSize:20
-       
+    botmessage: {
+        color: "orange",
+        fontSize: 20
     },
-  
-  });
+    btn: {
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        placeholderTextColor: 'gray',
+    },
+    textfield: {
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        placeholderTextColor: 'gray',
+        width: "100%",
+        padding: 5,
+    }
+});
--- components/Home.js
+++ components/Home.js
@@ -1,17 +1,16 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 
-export  function Home({navigation}) {
-const users=[
-
-    {id:1,name:"Faizan"},
-    {id:2,name:"Hassan"}
-]
+export function Home({ navigation }) {
+    const users = [
+        { id: 1, name: "Faizan" },
+        { id: 2, name: "Hassan" }
+    ]
     return (
-        <View style={{borderBottom:"1px solid purple"}}>
-           {users.map((user)=>{
-             return  <Text key={user.id} style={{fontSize:15, paddingLeft:10, borderTop:"1px solid purple", paddingTop:10, paddingBottom:10}} onPress={()=>navigation.navigate('Chat',{id:user.id,name:user.name})} >{user.name}</Text>
-           })}
+        <View style={{ borderBottom: "1px solid purple" }}>
+            {users.map((user) => {
+                return <Text key={user.id} style={{ fontSize: 15, paddingLeft: 10, borderTop: "1px solid purple", paddingTop: 10, paddingBottom: 10 }} onPress={() => navigation.navigate('Chat', { id: user.id, name: user.name })} >{user.name}</Text>
+            })}
         </View>
     )
 }
